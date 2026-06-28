77335d9f24a15560bf631e9fc2f3ee1433f13863
ahmad rafiansyah <arafiansyah20@gmail.com>
2026-06-29 00:12:32 +0700

    perbaikans server STUN

--- apps/frontend-nextjs/src/services/webrtc.service.ts
+++ apps/frontend-nextjs/src/services/webrtc.service.ts
@@ -151,7 +151,23 @@ export class WebRtcService {
 
         const config: RTCConfiguration = {
             iceServers: this.iceServers.length > 0 ? this.iceServers : [
-                { urls: 'stun:stun.l.google.com:19302' }
+                { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:openrelay.metered.ca:80' },
+                {
+                    urls: 'turn:openrelay.metered.ca:80',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                },
+                {
+                    urls: 'turn:openrelay.metered.ca:443',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                },
+                {
+                    urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                }
             ]
         };
 
